f2afbfe14c7c748b20741b20d4e203c7016cb566
zohaib68 <zohaibsarwar712@gmail.com>
2026-05-01 21:12:46 +0500

    fix: improved homepage styles

--- components/landing/molecules/featureCard.tsx
+++ components/landing/molecules/featureCard.tsx
@@ -11,9 +11,9 @@ export const FeatureCard = ({
     dividerClassName
 }: FeatureCardProps) => {
     return (
-        <div className={clsx("justify-center items-center  text-base-content/70 text-center rounded-0  flex flex-col p-2 bg-white/10  border border-white/10 ", className)}>
+        <div className={clsx("justify-center items-center  text-base-content/70 text-center rounded-0  flex flex-col gap-2 p-2 bg-white/10  border border-white/10 ", className)}>
             <div className="flex justify-center items-center gap-2">
-                <h3 className="text-lg font-semibold  font-sans">{title}</h3>
+                <h3 className="text-2xl font-semibold  font-sans">{title}</h3>
                 <div className="flex justify-center items-center">
                     <div className={clsx("w-10 h-0.5  rounded-full", dividerClassName)}>
 
@@ -24,7 +24,7 @@ export const FeatureCard = ({
                 </div>
 
             </div>
-            <p className="font-semibold font-sans">
+            <p className="text-lg  font-sans">
                 {description}
             </p>
         </div>
--- components/landing/organisms/featuresSection.tsx
+++ components/landing/organisms/featuresSection.tsx
@@ -8,13 +8,13 @@ export const FeaturesSection = () => {
                 Everything you need
             </h2>
 
-            <div className="grid md:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                 <FeatureCard
                     title="Task Management"
                     description="Track tasks with status and filters."
                     icon="clarity:tasks-line"
-                    className="h-28 bg-info/20! text-info-content rounded-bl-full! rounded-tr-full"
+                    className="shadow-info/30 shadow-md h-32 bg-info/50! text-info-content/90 rounded-bl-full! rounded-tr-full"
                     dividerClassName="bg-info/20 "
                     iconContainerClassName="bg-info/20!"
                 />
@@ -22,7 +22,7 @@ export const FeaturesSection = () => {
                     title="Calculator"
                     description="Quick calculations anytime."
                     icon='bi:calculator'
-                    className="h-32 bg-secondary/20! text-secondary-content  rounded-t-full! rounded-tl-full"
+                    className=" shadow-secondary/30 shadow-lg h-32 bg-secondary/50! text-secondary-content/90  rounded-tl-full! rounded-br-full"
                     dividerClassName="bg-info/20"
                     iconContainerClassName="bg-info/20!"
                 />
@@ -30,7 +30,7 @@ export const FeaturesSection = () => {
                     title="Notes"
                     description="Capture and organize ideas easily."
                     icon="gg:notes"
-                    className="h-44 bg-primary/20! text-primary-content rounded-br-full! rounded-tl-full"
+                    className="shadow-primary/30   shadow-md  md:col-span-2 h-40 bg-primary/80! text-primary-content/90 rounded-br-full! rounded-tl-full"
                     dividerClassName="bg-info/20"
                     iconContainerClassName="bg-info/20!"
                 />
